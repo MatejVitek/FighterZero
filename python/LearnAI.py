463704e5474bd05f757a5a6bb5f17db0f5c95694
Matej Vitek <matej.vitek@hotmail.com>
2018-06-11 14:01:36 +0200

    Basic frame completed for python agent

--- python/LearnAI.py
+++ python/LearnAI.py
@@ -1,24 +1,11 @@
+from keras.models import load_model
+
+
 class LearnAI(object):
-	def __init__(self, gateway):
+	def __init__(self, gateway, init_file=None, save_file=None):
 		self.gateway = gateway
-
-	def close(self):
-		pass
-
-	def getInformation(self, frameData):
-		# Getting the frame data of the current frame
-		self.frameData = frameData
-		self.cc.setFrameData(self.frameData, self.player)
-
-	# please define this method when you use FightingICE version 3.20 or later
-	def roundEnd(self, x, y, z):
-		print(x)
-		print(y)
-		print(z)
-
-	# please define this method when you use FightingICE version 4.00 or later
-	def getScreenData(self, sd):
-		pass
+		self._init = init_file
+		self._save = save_file
 
 	def initialize(self, gameData, player):
 		# Initializng the command center, the simulator and some other things
@@ -30,11 +17,14 @@ class LearnAI(object):
 		self.gameData = gameData
 		self.simulator = self.gameData.getSimulator()
 
+		self.nn = self.build_nn()
+
 		return 0
 
-	def input(self):
-		# Return the input for the current frame
-		return self.inputKey
+	def getInformation(self, frameData):
+		# Getting the frame data of the current frame
+		self.frameData = frameData
+		self.cc.setFrameData(self.frameData, self.player)
 
 	def processing(self):
 		# Just compute the input for the current frame
@@ -52,7 +42,33 @@ class LearnAI(object):
 		# Just spam kick
 		self.cc.commandCall("B")
 
+	def input(self):
+		# Return the input for the current frame
+		return self.inputKey
+
+	# please define this method when you use FightingICE version 3.20 or later
+	def roundEnd(self, x, y, z):
+		print(x)
+		print(y)
+		print(z)
+
+	def close(self):
+		if self._save is not None:
+			self.nn.save(self._save)
+
+	def build_nn(self):
+		return self._build_new() if self._init is None else self._init_nn()
+
+	def _build_new(self):
+		pass
+
+	def _init_nn(self):
+		return load_model(self._init)
+
+	# please define this method when you use FightingICE version 4.00 or later
+	def getScreenData(self, sd):
+		pass
+
 	# This part is mandatory
 	class Java:
 		implements = ["aiinterface.AIInterface"]
-
--- python/Main.py
+++ python/Main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import argparse
 
 from LearnAI import LearnAI
@@ -6,71 +7,128 @@ from PlayAI import PlayAI
 from py4j.java_gateway import JavaGateway, GatewayParameters as GParams, CallbackServerParameters as CSParams
 
 ALL_CHARS = "ZEN", "GARNET", "LUD"
+DEF_NN_FILE = 'MyNN.h5'
 
 # Game settings
 # "RND" for full random, random.choice(ALL_CHARS) for initial random
-CHARS = "ZEN", "ZEN"
 GAME_NUM = 2
-AI1 = AI2 = PlayAI
-FILE1 = FILE2 = 'MyAI.nn'
+CHARS = ["ZEN", "ZEN"]
+AI = [PlayAI, PlayAI]
+INIFILES = [DEF_NN_FILE, DEF_NN_FILE]
+SAVEFILES = [None, None]
+SAVEFILE = None
 
 
 def main():
-	start_game()
+	process_command_line_options()
+	print(AI, INIFILES, SAVEFILES, SAVEFILE, sep='\t')
+	run_game()
 	close_gateway()
 
 
 def process_command_line_options():
-	global AI1, AI2, FILE1, FILE2
+	global GAME_NUM, CHARS, AI, INIFILES, SAVEFILES, SAVEFILE
 
-	ap = argparse.ArgumentParser(description="Start a game of FGAI with the specified AI players.")
+	ap = argparse.ArgumentParser(description="Start a game of FGAI.")
+	ap.add_argument('-n', '--number', help="number of games to play")
+	ap.add_argument('-c1', '--character1', help="character for P1")
+	ap.add_argument('-c2', '--character2', help="character for P2")
 	ap.add_argument('-l', '--learn', action='store_true', help="use LearnAI for both players")
-	ap.add_argument('-f', '--file', help="initialise networks from specified file (for use with default AIs)")
-	ap.add_argument('-p1', '-ai1', '--ai1', '-AI1', '--AI1', '--player1', nargs=2, dest='ai1',
-					help="AI to use for P1; can be module.Class or just Class if module and class names are the same")
-	ap.add_argument('-p2', '-ai2', '--ai2', '-AI2', '--AI2', '--player2', nargs=2, dest='ai2',
-					help="AI to use for P2; can be module.Class or just Class if module and class names are the same")
-	ap.add_argument('-f1', '--file1', help="initialise network for P1 from specified file (for use with default AIs)")
-	ap.add_argument('-f2', '--file2', help="initialise network for P2 from specified file (for use with default AIs)")
+	ap.add_argument('-p1', '-ai1', '--ai1', '-AI1', '--AI1', '--player1', dest='ai1',
+	                help="AI to use for P1; can be module.Class or just Class if module and class names are the same")
+	ap.add_argument('-p2', '-ai2', '--ai2', '-AI2', '--AI2', '--player2', dest='ai2',
+	                help="AI to use for P2; can be module.Class or just Class if module and class names are the same")
+	ap.add_argument('-f', '--file', help="initialise both NNs from specified file (for use with default AIs)")
+	ap.add_argument('-f1', '--file1', help="initialise NN for P1 from specified file (for use with default AIs)")
+	ap.add_argument('-f2', '--file2', help="initialise NN for P2 from specified file (for use with default AIs)")
+	ap.add_argument('-s', '--save', help="save better NN to specified file at the end (for use with LearnAI)")
+	ap.add_argument('-s1', '--save1', help="save P1's NN to specified file at the end (for use with LearnAI)")
+	ap.add_argument('-s2', '--save2', help="save P2's NN to specified file at the end (for use with LearnAI)")
 
 	args = ap.parse_args()
 
+	if args.number:
+		GAME_NUM = args.number
+	if args.character1:
+		CHARS[0] = args.character1
+	if args.character2:
+		CHARS[1] = args.character2
+
 	if args.learn:
-		AI1 = AI2 = LearnAI
-		FILE1 = FILE2 = None
+		for i in (0, 1):
+			AI[i] = LearnAI
+			INIFILES[i] = None
+			SAVEFILE = DEF_NN_FILE
 	else:
 		if args.ai1:
-			AI1 = resolve_import_name(args.ai1)
+			AI[0] = resolve_import_name(args.ai1)
+			INIFILES[0] = None
+			SAVEFILES[0] = DEF_NN_FILE
 		if args.ai2:
-			AI2 = resolve_import_name(args.ai2)
+			AI[1] = resolve_import_name(args.ai2)
+			INIFILES[1] = None
+			SAVEFILES[1] = DEF_NN_FILE
+
 	if args.file:
-		FILE1 = FILE2 = args.file
+		for i in (0, 1):
+			INIFILES[i] = args.file
+		SAVEFILE = args.file
 	else:
 		if args.file1:
-			FILE1 = args.file1
+			INIFILES[0] = SAVEFILES[0] = args.file1
 		if args.file2:
-			FILE2 = args.file2
+			INIFILES[1] = SAVEFILES[1] = args.file2
+
+	if args.save:
+		SAVEFILE = args.save
+	if args.save1:
+		SAVEFILES[0] = args.save1
+	if args.save2:
+		SAVEFILES[1] = args.save2
 
 
 def resolve_import_name(name):
 	import importlib
-	m, c = name.split('.') if '.' in name else name, name
+	m, c = name.split('.') if '.' in name else (name, name)
 	return getattr(importlib.import_module(m), c)
 
 
-def start_game():
-	p1 = AI1(gateway)
-	p2 = AI2(gateway)
-	manager.registerAI(AI1.__name__, p1)
-	manager.registerAI(AI2.__name__, p2)
+def run_game():
+	os.chdir(".\\networks\\")
+
+	temp_saves = [None, None]
+	for i in (0, 1):
+		if AI[i] == LearnAI:
+			if SAVEFILES[i] is None:
+				temp_saves[i] = SAVEFILE[::-1].replace(".", ".%d_" % (i+1), 1)[::-1]
+				p = AI[i](gateway, INIFILES[i], temp_saves[i])
+			else:
+				p = AI[i](gateway, INIFILES[i], SAVEFILES[i])
+		elif AI[i] == PlayAI:
+			p = AI[i](gateway, INIFILES[i])
+		else:
+			p = AI[i](gateway)
+		manager.registerAI(AI[i].__name__, p)
 
 	print("Starting game")
 
-	game = manager.createGame(*CHARS, AI1.__name__, AI2.__name__, GAME_NUM)
+	game = manager.createGame(*CHARS, AI[0].__name__, AI[1].__name__, GAME_NUM)
 	manager.runGame(game)
+
+	if AI[0] == AI[1] == LearnAI and SAVEFILE is not None:
+		nn1 = SAVEFILES[0] if SAVEFILES[0] is not None else temp_saves[0]
+		nn2 = SAVEFILES[1] if SAVEFILES[1] is not None else temp_saves[1]
+		save_better_network(SAVEFILE, nn1, nn2)
+	for i in (0, 1):
+		if temp_saves[i] is not None:
+			os.remove(temp_saves[i])
 
 	print("Game ending")
 	sys.stdout.flush()
+
+
+def save_better_network(save, nn1, nn2):
+	pass
 
 
 def close_gateway():
@@ -82,6 +140,4 @@ gateway = JavaGateway(gateway_parameters=GParams(port=4242), callback_server_par
 manager = gateway.entry_point
 
 if __name__ == '__main__':
-	process_command_line_options()
-
-main()
+	main()
